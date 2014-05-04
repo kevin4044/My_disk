f48a6dac6c4c7c399005493b892d2c57e54caa6a
John Anderson <sontek@gmail.com>
2014-05-04 16:33:23 +0800

    397521# 【网盘】未完成功能：文件名称搜索、排序 排序功能。 补充遗漏文件

---
+++ files/js/sort.js
@@ -0,0 +1,44 @@
+/**
+ * Created by wangjunlong on 14-5-4.
+ */
+
+MYsort ={
+    data_container:'',
+    sort:function (key_class, order) {
+        var $data_container = $(this.data_container);
+        var $each = $data_container.children();
+        var dom_array = new Array();
+
+        $each.each(function () {
+            var obj = new Object();
+            obj.key = $(this).find(key_class).text();
+            obj.content = $(this)[0].outerHTML;
+
+            //todo:没有考虑属性名重复的情况
+            dom_array.push(obj);
+        })
+        dom_array.sort(function(a, b){
+            if (isNaN(parseInt(a.key)) === false
+                && isNaN(parseInt(b.key)) === false) {
+                if (order = 'asc') {
+                    return parseInt(b.key)-parseInt(a.key);
+                } else {
+                    return parseInt(a.key)-parseInt(b.key);
+                }
+
+            } else {
+                if (order = 'asc') {
+                    return b.key.localeCompare(a.key);
+                } else {
+                    return a.key.localeCompare(b.key);
+                }
+            }
+        });
+
+        $data_container.html("");
+        for (key in dom_array) {
+            $data_container.append(dom_array[key].content);
+        }
+    }
+
+}
